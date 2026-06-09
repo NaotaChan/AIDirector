59a54571abee9dbb3037a9d911ebf75116efb98f
Roberto <robertogolia17@gmail.com>
2026-06-09 23:52:24 +0200

    feat: removed all blueprint implementation and moved to code

--- Source/Project_AIDirector/Private/AI/AI_BaseController.cpp
+++ Source/Project_AIDirector/Private/AI/AI_BaseController.cpp
@@ -2,11 +2,35 @@
 
 
 #include "Project_AIDirector/Public/AI/AI_BaseController.h"
+#include "Navigation/CrowdFollowingComponent.h"
 #include "Project_AIDirector/Public/AI/AI_BaseNPC.h"
+#include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+
+AAI_BaseController::AAI_BaseController(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer.SetDefaultSubobjectClass<UPathFollowingComponent>(TEXT("PathFollowingComponent")))
+{
+	//Setup AI Path Following Quality if using Crowd
+	
+	if (UCrowdFollowingComponent* CrowdComponent = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+	{
+		CrowdComponent->SetCrowdAvoidanceQuality(ECrowdAvoidanceQuality::High);
+	}
+	
+	//Setup AI Perception
+	PerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AI Perception Component"));
+}
 
 void AAI_BaseController::OnPossess(APawn* InPawn)
 {
 	NPCRef = Cast<AAI_BaseNPC>(InPawn);
+	
 	Super::OnPossess(InPawn);
+	
+	PerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &AAI_BaseController::ActorPerceivedUpdate);
 }
 
+void AAI_BaseController::ActorPerceivedUpdate_Implementation(AActor* UpdatedActor, FAIStimulus Stimulus)
+{
+	
+}
--- Source/Project_AIDirector/Public/AI/AI_BaseController.h
+++ Source/Project_AIDirector/Public/AI/AI_BaseController.h
@@ -14,8 +14,14 @@ class PROJECT_AIDIRECTOR_API AAI_BaseController : public AAIController
 	GENERATED_BODY()
 
 public:	
+	
+	AAI_BaseController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
 	UFUNCTION(BlueprintPure, Category = "NPC")
 	AAI_BaseNPC* GetNPCRef() {return NPCRef;}
+	
+	UFUNCTION(BlueprintNativeEvent)
+	void ActorPerceivedUpdate(AActor* UpdatedActor, FAIStimulus Stimulus);
 
 protected:
 	virtual void OnPossess(APawn* InPawn) override;
