76f83d861d9b3fbcf55728fa7237ffcc92e4bcd7
Roberto <robertogolia17@gmail.com>
2026-06-16 10:22:55 +0200

    feat: custom heat sense fully c++ implemented

--- Source/Project_AIDirector/Private/AI/AISenseConfig_Heat.cpp
+++ Source/Project_AIDirector/Private/AI/AISenseConfig_Heat.cpp
@@ -2,3 +2,20 @@
 
 
 #include "AI/AISenseConfig_Heat.h"
+#include "AI/AISense_Heat.h"
+
+
+UAISenseConfig_Heat::UAISenseConfig_Heat(const FObjectInitializer& ObjectInitializer)
+{
+	SuccessDebugColor = FColor::Green;
+	DebugColor = SuccessDebugColor;
+	FailDebugColor = FColor::Red;
+	Implementation = GetSenseImplementation();
+}
+
+
+TSubclassOf<UAISense> UAISenseConfig_Heat::GetSenseImplementation() const
+{
+	return UAISense_Heat::StaticClass();
+}
+
--- Source/Project_AIDirector/Public/AI/AISenseConfig_Heat.h
+++ Source/Project_AIDirector/Public/AI/AISenseConfig_Heat.h
@@ -6,11 +6,42 @@
 #include "Perception/AISenseConfig.h"
 #include "AISenseConfig_Heat.generated.h"
 
-/**
- * 
- */
-UCLASS()
+class UAISense_Heat;
+
+UCLASS(meta = (DisplayName = "AI Heat config"))
 class PROJECT_AIDIRECTOR_API UAISenseConfig_Heat : public UAISenseConfig
 {
 	GENERATED_BODY()
+	
+public:
+	
+	UAISenseConfig_Heat(const FObjectInitializer& ObjectInitializer);
+	
+	//The class reference which contains the logic for this sense config
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sense", NoClear, config)
+	TSubclassOf<UAISense_Heat> Implementation;
+	
+	//The radius around the pawn that we're checking for "water resources"
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sense", config, meta = (UIMin = 0.0, Clampmin = 0.0))
+	float HeatRadius;
+	
+	//True if you want to display the success line debug
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sense", config)
+	bool bDisplaySuccessDebug;
+	
+	//Not used as UPROPERTY because it is used only to copy the Protected Debug Color variable
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sense", config)
+	FColor SuccessDebugColor;
+	
+	//True if you want to display the fail line debug
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sense", config)
+	bool bDisplayFailDebug;
+	
+	//Not used as UPROPERTY because it is used only to copy the Protected Debug Color variable
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Sense", config)
+	FColor FailDebugColor;
+	
+	
+	virtual TSubclassOf<UAISense> GetSenseImplementation() const override;
+	
 };
