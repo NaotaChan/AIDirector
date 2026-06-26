f08c9fb9d8a7a5428ce3f87f6808e268998ef6bb
Roberto <robertogolia17@gmail.com>
2026-06-26 11:19:21 +0200

    feat: classe name correction and change state implementation

---
+++ Source/Project_AIDirector/Private/Components/AwarenessComponent.cpp
@@ -0,0 +1,97 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/AwarenessComponent.h"
+#include "AI/AI_BaseController.h"
+#include "Components/AlertComponent.h"
+
+
+
+// Sets default values for this component's properties
+UAwarenessComponent::UAwarenessComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	bAutoActivate = true;
+	bWantsInitializeComponent = true;
+}
+
+void UAwarenessComponent::InitializeComponent()
+{
+	AICRef = Cast<AAI_BaseController>(GetOwner());
+	
+	Super::InitializeComponent();
+}
+
+void UAwarenessComponent::UpdateAwarenessValue(float AwarenessDelta)
+{
+	if (IsValid(AICRef) && GetCanUpdateAwareness())
+	{
+		if (AwarenessDelta > 0)
+		{
+			//Restart the timer if the stimulus persist
+			if (PauseTimerHandle.IsValid())
+			{
+				GetWorld()->GetTimerManager().ClearTimer(PauseTimerHandle);
+			}
+			
+			GetWorld()->GetTimerManager().SetTimer(PauseTimerHandle, this, &UAwarenessComponent::OnPauseFinished, AwarenessPauseTime, false);
+			
+		}
+		
+		CurrentAwarenessValue = FMath::Clamp(CurrentAwarenessValue + AwarenessDelta, 0.f, MaxAwarenessValue);
+		
+		if (CurrentAwarenessValue >= MaxAwarenessValue)
+		{
+			CurrentAwarenessValue = MaxAwarenessValue;
+			
+			SetCanUpdateAwareness(false);
+			
+			if (IsValid(AICRef->GetAlertComponent()))
+			{
+				AICRef->GetAlertComponent()->SetCanUpdateAlerted(true);
+				AICRef->UpdateCurrentStatusTag(E_AITag::SUSPICIOUS);
+			}
+		}
+	}
+}
+
+
+void UAwarenessComponent::OnPauseFinished()
+{
+	GetWorld()->GetTimerManager().ClearTimer(PauseTimerHandle);
+	
+	if (!GetIsDecreasing())
+	{
+		SetIsDecreasing(true);
+		OnDecreaseAwarenessValue();
+		GetWorld()->GetTimerManager().SetTimer(DecreaseTimerHandle, this, &UAwarenessComponent::OnDecreaseAwarenessValue, AwarenessDecreaseTime, true);
+	}
+}
+
+void UAwarenessComponent::OnDecreaseAwarenessValue()
+{
+	if (GetIsDecreasing())
+	{
+		UpdateAwarenessValue(-AwarenessDecreaseValue);
+		
+		if (CurrentAwarenessValue <= 0)
+		{
+			CurrentAwarenessValue = 0;
+			SetIsDecreasing(false);
+			GetWorld()->GetTimerManager().ClearTimer(DecreaseTimerHandle);
+		}
+	}
+}
+
+// Called when the game starts
+void UAwarenessComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+
+
---
+++ Source/Project_AIDirector/Public/Components/AwarenessComponent.h
@@ -0,0 +1,64 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AwarenessComponent.generated.h"
+
+class AAI_BaseController;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECT_AIDIRECTOR_API UAwarenessComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAwarenessComponent();
+	
+	virtual void InitializeComponent() override;
+	
+	UFUNCTION()
+	bool GetCanUpdateAwareness() const {return CanUpdateAwareness;}
+	
+	UFUNCTION()
+	void SetCanUpdateAwareness(bool NewStatus) {CanUpdateAwareness = NewStatus;}
+	
+	UFUNCTION()
+	bool GetIsDecreasing() const {return IsDecreasing;}
+	
+	UFUNCTION()
+	void SetIsDecreasing(bool NewStatus) {IsDecreasing = NewStatus;}
+	
+	UFUNCTION(BlueprintCallable, Category = "Awareness")
+	void UpdateAwarenessValue(float AwarenessDelta);
+	
+	void OnPauseFinished();
+	
+	void OnDecreaseAwarenessValue();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	UPROPERTY()
+	AAI_BaseController* AICRef;
+	
+	FTimerHandle PauseTimerHandle;
+	
+	FTimerHandle DecreaseTimerHandle;
+	
+private:
+	
+	float CurrentAwarenessValue;
+	float MaxAwarenessValue;
+	float AwarenessDecreaseValue;
+	float AwarenessDecreaseTime;
+	float AwarenessPauseTime;
+	float AwarenessIncreaseOnHearing;
+	float AwarenessIncreaseOnSight;	
+	
+	bool CanUpdateAwareness;
+	bool IsDecreasing;
+};
