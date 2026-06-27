9ee35fd21d758b165373ae5eb2544c11aee79021
Roberto <robertogolia17@gmail.com>
2026-06-27 15:08:07 +0200

    feat: created enum for different sight cone zones (multi sight cone)

---
+++ Source/Project_AIDirector/Private/Enum/Perception/Sight/Enum_SightConeZones.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enum/Perception/Sight/Enum_SightConeZones.h"
+
+
+Enum_SightConeZones::Enum_SightConeZones()
+{
+}
+
+Enum_SightConeZones::~Enum_SightConeZones()
+{
+}
